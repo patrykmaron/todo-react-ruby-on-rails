1c20d6c6d79046ed8424e9181a5d306503545bf8
patrykmaron <up705461@myport.ac.uk>
2020-08-19 18:00:56 +0100

    installed typescript gem, configured react refresh, webpack for HMR

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,21 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const environment = require('./environment')
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-module.exports = environment.toWebpackConfig()
+const environment = require("./environment");
+
+const isWebpackDevServer = process.env.WEBPACK_DEV_SERVER;
+
+//plugins
+if (isWebpackDevServer) {
+  environment.plugins.append(
+    "ReactRefreshWebpackPlugin",
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockPort: 3035,
+      },
+    })
+  );
+}
+
+module.exports = environment.toWebpackConfig();
